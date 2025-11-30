0aaedd077dd574e8fc717ddb9f359b6696454a70
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-11-30 20:10:54 +0300

    feat: add metadata

--- src/app/(landing)/reviews/page.tsx
+++ src/app/(landing)/reviews/page.tsx
@@ -7,7 +7,7 @@ const reviews = [
     id: 1,
     author: {
       name: "Anthony Bruff",
-      image: "/avatars/Anthony.jpg",
+      image: "/avatars/anthony.jpg",
     },
     date: "2 Mar. 2025",
     text: "The service at the Hotel Monteleone was exceptional. There was absolutely no issue that was not addressed timely and with satisfactory results. We were particulary impressed with how the hotel staff anticipated our needs (periodically coming by the Board Room to check with us). Numerous conference attendees commented on the quality of the food, the quality of the service and overall positive attitude toward the conference site. Particular noteworthy is the longevity of the staff and that sense of investment in the success of every event. I usually offer suggestions for improvements (part of being a marketing professor), but there is absolutely nothing that could be improved – you have set the bar very high.",
